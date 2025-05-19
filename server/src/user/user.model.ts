4cc92fb60a7dfc6b1e11aaaa8c0f2b91e2b2b860
Igomigo <igomigofatai@gmail.com>
2025-05-19 23:06:01 +0100

    Feat: Add user model

---
+++ server/src/user/user.model.ts
@@ -0,0 +1,45 @@
+import { getModelForClass, ModelOptions, Prop } from "@typegoose/typegoose";
+import mongoose from "mongoose";
+
+class Location {
+    @Prop({ required: true })
+    country!: string;
+
+    @Prop({ required: true })
+    state!: string;
+
+    @Prop({ required: true })
+    city!: string;
+}
+
+@ModelOptions({
+    schemaOptions: {
+        timestamps: true,
+        versionKey: false
+    }
+})
+class User {
+    _id!: mongoose.Types.ObjectId;
+
+    @Prop({ required: true, unique: true })
+    username!: string;
+
+    @Prop({ required: true, unique: true })
+    email!: string;
+
+    @Prop({ required: true })
+    password!: string;
+
+    @Prop({ type: () => Location,required: true })
+    location!: Location;
+
+    @Prop({ type: [mongoose.Types.ObjectId], ref: "Vendor", default: [] })
+    favourites?: mongoose.Types.ObjectId[];
+
+    @Prop({ required: false, default: false })
+    isVerified?: boolean;
+}
+
+const UserModel = getModelForClass(User);
+
+export {UserModel, User};
