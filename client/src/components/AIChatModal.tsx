9776fee0e356b89329f8bacd1f7bb7b1d7aed42f
Igomigo <igomigofatai@gmail.com>
2025-08-17 13:13:33 +0100

    Update package dependencies and enhance AIChatModal: Add new dependencies for improved UI components, including @icons-pack/react-simple-icons and @radix-ui/react-collapsible. Refactor chat history structure and integrate AI conversation components for better user interaction.

--- client/src/components/AIChatModal.tsx
+++ client/src/components/AIChatModal.tsx
@@ -8,9 +8,20 @@ import {
 } from "./ui/dialog";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
-import { Sparkles } from "lucide-react";
+import { Bot, Sparkles } from "lucide-react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Button } from "./ui/button";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import {
+  AIConversation,
+  AIConversationContent,
+  AIConversationScrollButton,
+} from "./ui/shadcn-io/ai/conversation";
+import {
+  AIMessage,
+  AIMessageAvatar,
+  AIMessageContent,
+} from "./ui/shadcn-io/ai/message";
 
 // Chat Suggestions
 const chatSuggestions = [
@@ -19,24 +30,64 @@ const chatSuggestions = [
   "What vendors sell rice in my location?",
 ];
 
+type ChatHistory = {
+  role: "user" | "assistant";
+  content: string;
+  avatar: string;
+  createdAt: Date;
+};
+
 // Chat History
-const chatHistory = [
+const chatHistory: ChatHistory[] = [
   {
     role: "user",
     content: "What is the best food in Lagos?",
+    avatar: "images/user.png",
+    createdAt: new Date(),
   },
   {
     role: "assistant",
     content:
       "The best food in Lagos is Jollof Rice. I can help you find the best food in Lagos.",
+    avatar: "images/bot.png",
+    createdAt: new Date(),
   },
   {
     role: "user",
     content: "What is the best food in Abuja?",
+    avatar: "images/user.png",
+    createdAt: new Date(),
   },
   {
     role: "assistant",
     content: "The best food in Abuja is Suya",
+    avatar: "images/bot.png",
+    createdAt: new Date(),
+  },
+  {
+    role: "user",
+    content: "What is the best food in Lagos?",
+    avatar: "images/user.png",
+    createdAt: new Date(),
+  },
+  {
+    role: "assistant",
+    content:
+      "The best food in Lagos is Jollof Rice. I can help you find the best food in Lagos.",
+    avatar: "images/bot.png",
+    createdAt: new Date(),
+  },
+  {
+    role: "user",
+    content: "What is the best food in Abuja?",
+    avatar: "images/user.png",
+    createdAt: new Date(),
+  },
+  {
+    role: "assistant",
+    content: "The best food in Abuja is Suya",
+    avatar: "images/bot.png",
+    createdAt: new Date(),
   },
 ];
 
@@ -49,50 +100,29 @@ export default function AIChatModal({
 }) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl p-0 m-3">
-        <Card className="border-white w-full">
-          <CardHeader>
-            <DialogTitle>
-              <div className="flex items-center gap-2">
-                <Sparkles className="w-6 h-6 text-orange-500" />
-                <h2 className="text-xl font-bold">Foodie AI</h2>
-                <Badge variant={"secondary"}>AI Assistant</Badge>
-              </div>
-            </DialogTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col gap-4 pb-4">
-              <div className="bg-gray-300 w-full px-4 py-2 rounded-lg">
-                <p className="text-sm">
-                  Hi! I'm your AI food assistant. I can help you find the
-                  perfect African dishes based on your mood, location, and
-                  preferences. What are you craving today?
-                </p>
-              </div>
-              {/** Chat Suggestions */}
-              <div className="flex items-center gap-2">
-                {chatSuggestions.map((suggestion) => (
-                  <Button
-                    className="text-xs rounded-full hover:ring-4 hover:ring-offset-1 hover:ring-gray-300"
-                    key={suggestion}
-                  >
-                    {suggestion}
-                  </Button>
-                ))}
-              </div>
-              {/** Chat container */}
-              <div className="flex flex-col gap-4">
-                <div></div>
-              </div>
+      <DialogContent className="max-w-4xl m-3">
+        <DialogHeader>
+          <DialogTitle>
+            <div className="flex items-center gap-2">
+              <Bot className="text-orange-500 w-6 h-6" />
+              <h2 className="text-xl font-bold">Foodie AI</h2>
+              <Badge variant={"secondary"}>AI Assistant</Badge>
             </div>
-            {/* <DotLottieReact
-              src="https://lottie.host/656889b5-09ae-40c6-9a76-ada6e367d02c/MXWWG6jO7B.lottie"
-              loop
-              autoplay
-              className="w-20 h-10"
-            /> */}
-          </CardContent>
-        </Card>
+          </DialogTitle>
+        </DialogHeader>
+        <AIConversation>
+          <AIConversationContent>
+            {chatHistory.map((chat, index) => (
+              <AIMessage key={index} from={chat.role}>
+                <AIMessageContent>
+                  <p>{chat.content}</p>
+                </AIMessageContent>
+                <AIMessageAvatar src={chat.avatar} name={chat.role} />
+              </AIMessage>
+            ))}
+          </AIConversationContent>
+          <AIConversationScrollButton />
+        </AIConversation>
       </DialogContent>
     </Dialog>
   );
